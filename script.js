67654cdf26bbd1de6d2d9e2fb41023e6d72ab350
Mitch Malinin <mitchmalinin@gmail.com>
2021-04-25 20:36:21 -0400

    mobile nav working

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleBurger = () => {
+  document.querySelector(".burger").classList.toggle("open")
+  document.querySelector(".nav").classList.toggle("open")
+  document.querySelector(".mobile-nav-bg").classList.toggle("open")
+}
